fb59dbb4880490b963073788be4a77c74699d811
Chris Neal <voodoodaddy2000@gmail.com>
2019-11-05 09:55:26 -0700

    Questions are being asked

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 const questions = [
-  
+    {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub Username?",
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "fav_color",
+        choices: ["Green", "Blue", "Pink", "Red"]
+    }
 ];
 
-function writeToFile(fileName, data) {
- 
-}
-
 function init() {
+    inquirer.prompt(questions)
+    .then(function(response) {
+        console.log(response);
+        const fileData = JSON.stringify(response, null, 2)
+        fs.writeFile("test.json", fileData, function(err){
+          console.log(err);
+        })
+    });
+}
 
 init();
